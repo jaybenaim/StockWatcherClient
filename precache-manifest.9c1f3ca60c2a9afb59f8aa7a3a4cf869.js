e38b7e749ea8e72cbc8cd0a796f77d0c77ba5cd8
jacobbenaim <jacobjbenaim@gmail.com>
2021-08-30 13:53:51 -0400

    Updates

--- precache-manifest.9c1f3ca60c2a9afb59f8aa7a3a4cf869.js
+++ precache-manifest.9c1f3ca60c2a9afb59f8aa7a3a4cf869.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5991427a8dc9fa74a7421451634efd56",
+    "revision": "2c29743e0f7eeb911cf9edfae7aa6ebc",
     "url": "/StockWatcherClient/index.html"
   },
   {
-    "revision": "3a3d44b61dfe00988911",
+    "revision": "9a9ca3fbd0d1ad8fea2b",
     "url": "/StockWatcherClient/static/css/main.d07236a7.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/StockWatcherClient/static/js/2.50baa2a9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a3d44b61dfe00988911",
-    "url": "/StockWatcherClient/static/js/main.54af8298.chunk.js"
+    "revision": "9a9ca3fbd0d1ad8fea2b",
+    "url": "/StockWatcherClient/static/js/main.0ba1b1af.chunk.js"
   },
   {
     "revision": "e50336665c812b0b28c0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/StockWatcherClient/precache-manifest.d1943da5b71d902bfd94b9ae7f1ab8d9.js"
+  "/StockWatcherClient/precache-manifest.9c1f3ca60c2a9afb59f8aa7a3a4cf869.js"
 );
 
 self.addEventListener('message', (event) => {
