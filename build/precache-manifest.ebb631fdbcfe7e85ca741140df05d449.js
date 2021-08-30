ed162c4aafb4e20c52bcd19b868c9f29850926ef
jacobbenaim <jacobjbenaim@gmail.com>
2021-08-30 09:09:00 -0400

    build

---
+++ build/precache-manifest.ebb631fdbcfe7e85ca741140df05d449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a3b1bf46047227fb27f3778638a450",
+    "url": "/StockWatcherClient/index.html"
+  },
+  {
+    "revision": "ca5adca0972ebac36aeb",
+    "url": "/StockWatcherClient/static/css/main.d07236a7.chunk.css"
+  },
+  {
+    "revision": "44a27ba3b2e7a7aca6d7",
+    "url": "/StockWatcherClient/static/js/2.9c6ff865.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/StockWatcherClient/static/js/2.9c6ff865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5adca0972ebac36aeb",
+    "url": "/StockWatcherClient/static/js/main.a4bdad20.chunk.js"
+  },
+  {
+    "revision": "e50336665c812b0b28c0",
+    "url": "/StockWatcherClient/static/js/runtime-main.e51d0e06.js"
+  }
+]);
