ce8e734e2cbc34ad032e39d35f4df861f7518e4c
jacobbenaim <jacobjbenaim@gmail.com>
2021-10-09 13:39:26 -0400

    removed sw

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ import "./config/firebase";
 import firebase from "firebase/app";
 
 import "assets/stylesheets/main.scss"
-import register from "registerServiceWorker";
+import { unregister } from "registerServiceWorker";
 
 const rrfConfig = {
   userProfile: "users",
@@ -44,4 +44,4 @@ ReactDOM.render(
  * Be aware that the website will only update to the latest version on the 2nd page visit if it as already cached
  * Learn more about service workers in React: https://create-react-app.dev/docs/making-a-progressive-web-app
  */
-register();
+unregister();
