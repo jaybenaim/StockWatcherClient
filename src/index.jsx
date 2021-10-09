434a74f8de4e1ba7eb2ca9d26e3ea2bdd42895d4
jacobbenaim <jacobjbenaim@gmail.com>
2021-10-08 23:58:01 -0400

    removed og sw

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
-import unregister from "./registerServiceWorker";
 import ErrorBoundary from "./components/Auth/ErrorBoundary";
 
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
@@ -44,4 +43,4 @@ ReactDOM.render(
  * Be aware that the website will only update to the latest version on the 2nd page visit if it as already cached
  * Learn more about service workers in React: https://create-react-app.dev/docs/making-a-progressive-web-app
  */
-unregister();
+// unregister();
