105a413824501477694c580113a3e9ab0d460977
jacobbenaim <jacobjbenaim@gmail.com>
2021-10-08 23:40:27 -0400

    test sw

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,39 +1,78 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+// /**
+//  * Welcome to your Workbox-powered service worker!
+//  *
+//  * You'll need to register this file in your web app and you should
+//  * disable HTTP caching for this file too.
+//  * See https://goo.gl/nhQhGp
+//  *
+//  * The rest of the code is auto-generated. Please don't update this file
+//  * directly; instead, make changes to your Workbox build configuration
+//  * and re-run your build process.
+//  * See https://goo.gl/2aRDsh
+//  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/StockWatcherClient/precache-manifest.56584397e796f10f189b0f3af29bed1e.js"
-);
+// importScripts(
+//   "/StockWatcherClient/precache-manifest.56584397e796f10f189b0f3af29bed1e.js"
+// );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+// self.addEventListener('message', (event) => {
+//   if (event.data && event.data.type === 'SKIP_WAITING') {
+//     self.skipWaiting();
+//   }
+// });
 
-workbox.core.clientsClaim();
+// workbox.core.clientsClaim();
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+// /**
+//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+//  * requests for URLs in the manifest.
+//  * See https://goo.gl/S9QRab
+//  */
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StockWatcherClient/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+// workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StockWatcherClient/index.html"), {
+
+//   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+// });
+import firebase from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import "firebase/storage";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/messaging";
+
+// eslint-disable-next-line
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+// eslint-disable-next-line
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBucy-UeTjedwPID39714EAHoA9R3H_GGs",
+  authDomain: "stockwatcher-67aec.firebaseapp.com",
+  databaseURL: "https://stockwatcher-67aec-default-rtdb.firebaseio.com/",
+  projectId: "stockwatcher-67aec",
+  storageBucket: "stockwatcher-67aec.appspot.com",
+  messagingSenderId: "422151887884",
+  appId: "1:422151887884:web:0b404f26f816da3d81a229",
+  measurementId: "G-HDMT81EJ18",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload){
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
