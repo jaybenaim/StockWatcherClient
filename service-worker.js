1c6cba271845e2f9c8e9cec81d334efed1387255
jacobbenaim <jacobjbenaim@gmail.com>
2021-10-09 22:50:11 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/StockWatcherClient/precache-manifest.1030c638dd3c7fd6e51b2cfa0d65067f.js"
+  "/StockWatcherClient/precache-manifest.32b2acf9e386590aacbe837f7606afae.js"
 );
 
 self.addEventListener('message', (event) => {
