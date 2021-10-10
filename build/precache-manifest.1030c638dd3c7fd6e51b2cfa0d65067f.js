1e3614f5d90edd5cb0affdbc79e0b8e6b8032eb3
jacobbenaim <jacobjbenaim@gmail.com>
2021-10-09 21:11:37 -0400

    removed action for gh

---
+++ build/precache-manifest.1030c638dd3c7fd6e51b2cfa0d65067f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da137c36304c3e869a0eb34ac023b7f",
+    "url": "/StockWatcherClient/index.html"
+  },
+  {
+    "revision": "1bce40518fb590be640e",
+    "url": "/StockWatcherClient/static/css/main.efa87f28.chunk.css"
+  },
+  {
+    "revision": "26798a4856d2c0eed704",
+    "url": "/StockWatcherClient/static/js/2.d790980d.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/StockWatcherClient/static/js/2.d790980d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bce40518fb590be640e",
+    "url": "/StockWatcherClient/static/js/main.21665abe.chunk.js"
+  },
+  {
+    "revision": "e50336665c812b0b28c0",
+    "url": "/StockWatcherClient/static/js/runtime-main.e51d0e06.js"
+  }
+]);
